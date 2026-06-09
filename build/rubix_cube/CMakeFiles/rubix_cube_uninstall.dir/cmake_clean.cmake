43d5b4a5b71d0a050242ffe45e602c30b1917b67
Alvin Park <alvinpark7412@gmail.com>
2026-06-09 11:25:45 -0700

    remove build artifacts, add .gitignore

--- build/rubix_cube/CMakeFiles/rubix_cube_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/rubix_cube_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/rubix_cube_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/rubix_cube/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/alvin/OneDrive/Documents/Uoft Folders/UofT 1st Year/Rubix cube/src/rubix_cube
-# Build directory: /mnt/c/Users/alvin/OneDrive/Documents/Uoft Folders/UofT 1st Year/Rubix cube/build/rubix_cube
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
